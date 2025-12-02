e4789090bf441ce21207965a1de976929588b803
Mateusz Górski <mateuszg18@gmail.com>
2025-12-02 21:53:34 +0100

    day 2 part 2, not complete

--- 2.py
+++ 2.py
@@ -65,14 +65,74 @@ def solution_1(data):
 
     return result
 
+def number_decomposition(number):
+    result = []
+    n = number
+    while n > 1:
+        i = 2
+        while True:
+            if n % i == 0:
+                result.append(i)
+                n = n // i
+                break
+            i += 1
+    return result
+
+
+def divisors_of_number(number):
+    i = 2
+    while i <= number:
+        if number % i == 0:
+            decomposition = number_decomposition(i)
+            if any([decomposition[j] == decomposition[j+1] for j in range(len(decomposition) - 2)]):
+                i += 1
+                continue
+            if len(decomposition) % 2 == 0:
+                sign = -1
+            else:
+                sign = 1
+            yield (sign, i)
+        i += 1
+
+def construct_divisor(num_of_ones, num_of_digits):
+    num_digits_per_one = num_of_digits // num_of_ones
+    result = 0
+
+    for _ in range(num_of_ones):
+        result *= 10 ** (num_digits_per_one)
+        result += 1
+
+    return result
+
 def sum_of_invalid_numbers_v2_in_interval(start, stop):
+    result = 0
 
+    num_digits = 2
+    start_small = 10
+    stop_small = 99
 
+    while stop > start_small:
+        overlap = get_intervals_overlap(start, stop, start_small, stop_small)
+        if overlap is None:
+            start_small *= 10
+            stop_small = stop_small * 10 + 9
+            num_digits += 1
+            continue
+
+        for sign, i in divisors_of_number(num_digits):
+            divisor = construct_divisor(i, num_digits) 
+            result += sign * sum_of_divisible_numbers(overlap[0], overlap[1], divisor)
+
+        start_small *= 10
+        stop_small = stop_small * 10 + 9
+        num_digits += 1
+
+    return result
 
 def solution_2(data):
     result = 0
     for (start, stop) in data:
-        result += sum_of_invalid_numbers_in_interval(start, stop)
+        result += sum_of_invalid_numbers_v2_in_interval(start, stop)
 
     return result
 
@@ -81,7 +141,7 @@ solution_idx = 2
 full_input = f"full_{solution_idx}"
 short_input = f"short_{solution_idx}"
 short_result_1 = 1227775554
-short_result_2 = -1
+short_result_2 = 4174379265
 
 SolutionImpl(full_input, short_input, short_result_1).run(solution_1)
 SolutionImpl(full_input, short_input, short_result_2).run(solution_2)
