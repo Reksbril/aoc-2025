33e07509e111cd997785edc57e8349fdddb2e552
Mateusz Górski <mateuszg18@gmail.com>
2025-12-05 17:28:46 +0100

    day 5 part 2

--- 5.py
+++ 5.py
@@ -38,16 +38,58 @@ def solution_2(data):
         idx = 0
 
         # look for first sorted range that we potentially overlap with
-        while True:
+        while idx < len(sorted_ranges):
             sr = sorted_ranges[idx]
-            if r[0] <= sr[1]:
+            if r[0] > sr[1]:
+                idx += 1
+            else:
+                break
+
+        if idx == len(sorted_ranges):
+            sorted_ranges.append(r)
+            continue
+
+        sr = sorted_ranges[idx]
+        # No overlap
+        if r[1] < sr[0]:
+            sorted_ranges.insert(idx, r)
+            continue
+
+        new_begin = min(r[0], sr[0])
+        new_end = max(r[1], sr[1])
+        ranges_to_remove = 1
+        first_range_to_remove = idx
+
+        idx += 1
+
+        # look for the last range that we overlap with
+        while idx < len(sorted_ranges):
+            sr = sorted_ranges[idx]
+            if new_end >= sr[0]:
+                new_end = max(new_end, sr[1])
+                ranges_to_remove += 1
+                idx += 1
+            else:
+                break
+
+        while ranges_to_remove > 0:
+            sorted_ranges.pop(first_range_to_remove)
+            ranges_to_remove -= 1
+
+        sorted_ranges.insert(first_range_to_remove, (new_begin, new_end))
+
+    result = 0
+    for sr in sorted_ranges:
+        result += sr[1] - sr[0] + 1
+
+    return result
 
 
 solution_idx = 5
 full_input = f"full_{solution_idx}"
 short_input = f"short_{solution_idx}"
 short_result_1 = 3
-short_result_2 = -1
+short_result_2 = 14
 
 SolutionImpl(full_input, short_input, short_result_1).run(solution_1)
 SolutionImpl(full_input, short_input, short_result_2).run(solution_2)
